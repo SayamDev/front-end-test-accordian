3b60085b941f976b35372a6630356b1737fbd658
Sayam Ajmal <itssanesayam@gmail.com>
2022-08-04 21:03:20 +0100

    triangle funcionality working

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ document.getElementById("toggleMenu2").addEventListener("click", toggleMenu2);
 document.getElementById("toggleMenu3").addEventListener("click", toggleMenu3);
 
 
+const triangle = document.getElementById("triangle");
+
 var toggleStatus = 1;
 		function toggleMenu() {
 			if (toggleStatus == 1) {
 				document.getElementById("menu").style.display = "block";
+				triangle.style.transform = "rotate(180deg)";
+				
+
 				toggleStatus = 0;
 			} else if (toggleStatus == 0) {
 				document.getElementById("menu").style.display = "none";
 				toggleStatus = 1;
+				triangle.style.transform = "rotate(360deg)";
 			}
 		}
 var toggleStatus = 1;
@@ -18,9 +24,12 @@ var toggleStatus = 1;
 			if (toggleStatus == 1) {
 				document.getElementById("menu2").style.display = "block";
 				toggleStatus = 0;
+				
 			} else if (toggleStatus == 0) {
 				document.getElementById("menu2").style.display = "none";
 				toggleStatus = 1;
+				
+				
 			}
 		}
 var toggleStatus = 1;
